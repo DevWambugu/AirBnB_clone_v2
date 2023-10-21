879891744d4a4c95829dd12682ec50f27fe36b72
DevWambugu <venusdata100@gmail.com>
2023-10-21 13:02:38 +0000

    reroute to hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''This script starts a web application
+The listens on 0.0.0.0, port 5000
+'''
+from flask import Flask
+
+
+app = Flask(__name__)
+'''Route is defined with strict_slashes=False'''
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''display “Hello HBNB!”'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''display “HBNB!”'''
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    '''listen on 0.0.0.0 port 5000'''
+    app.run(host="0.0.0.0", port=5000)
