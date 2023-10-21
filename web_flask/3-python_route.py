f3588756cba15721785378a4e33598f9ae26863d
DevWambugu <venusdata100@gmail.com>
2023-10-21 13:38:09 +0000

    for default text define parent path folder

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,6 +27,7 @@ def c_text(text):
     formatted_text = text.replace("_", " ")
     return "C " + formatted_text
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is_cool"):
