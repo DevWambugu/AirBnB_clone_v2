2aafe8c876a411f70de194c50e15a4b3e0b12b95
DevWambugu <venusdata100@gmail.com>
2023-10-21 13:59:01 +0000

    change syntax

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -36,11 +36,10 @@ def python_text(text="is_cool"):
     return "Python " + formatted_text
 
 
-@app.route('/number/<n>', strict_slashes=False)
-def n_int(n):
+@app.route('/number/<int:n>', strict_slashes=False)
+def check_int(n):
     '''display “n is a number” only if n is an integer'''
-    if isinstance(n, int):
-        return "n is a number"
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":
