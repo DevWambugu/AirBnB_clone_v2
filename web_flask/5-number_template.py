066a5b5a72680dd163c7255b9121f2cc70ab5684
DevWambugu <venusdata100@gmail.com>
2023-10-21 14:42:20 +0000

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,8 +43,8 @@ def check_int(n):
     return f"{n} is a number"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def HTML_int(n):
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
     '''Displays a HTML output if n is an integer'''
     return render_template("5-number.html", n=n)
 
